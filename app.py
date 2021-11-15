017d0b89646becb1efc92599b1a16d7dc1346943
wins0117 <32445436+wins0117@users.noreply.github.com>
2021-11-15 16:22:39 +0800

    update api

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from linebot.models import *
 
 app = Flask(__name__)
 # Channel Access Token
-line_bot_api = LineBotApi('your channel token')
+line_bot_api = LineBotApi('+qRQRA2D5y45IzqBaxQwSwHNC/UhOAF8VaCPHB6jyFmbGaB4L7R+lhGDK20OJxcWZHTswmciwbucdXx2IycoTcsjvMktiWooZe/hTzJHW+/NAkDlYlX4u25ZI17R6r9yK/lnVQLMIxDiAGNvch4BeQdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('your channel secret')
+handler = WebhookHandler('0d6d26b3d08db94b95f9ae1b2f40cb43')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
