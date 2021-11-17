7e477520678c6ebbb4f1a7c83fd2fc990f6f03fe
wins0117 <32445436+wins0117@users.noreply.github.com>
2021-11-17 14:17:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,38 +33,11 @@ def callback():
 # 處理訊息 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text.lower()
-
-    if msg == 'hi':
-        message = TemplateSendMessage(
-        alt_text='Buttons template',
-        template=ButtonsTemplate(
-        thumbnail_image_url='https://i0.wp.com/www.womstation.com/wp-content/uploads/2018/11/%E9%9F%93%E5%9C%8B4.png?w=1280&ssl=1',
-        title='第一個小功能',
-        text='測試雞肋功能',
-        actions=[
-            URITemplateAction(
-                label='熱門youtube',
-                uri='https://www.youtube.com/feed/trending'
-            ),
-            URITemplateAction(
-                label='小新聞',
-                uri='https://news.google.com/?hl=zh-TW&tab=wn1&gl=TW&ceid=TW:zh-Hant'
-            ),
-            URITemplateAction(
-                label='IGIG',
-                uri='https://www.instagram.com/?hl=zh-tw'
-            ),
-
-            ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token, message)
-
-    else:
-    	message = TextSendMessage(text='請輸入hi')
-    	line_bot_api.reply_message(event.reply_token, message)
+def echo(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
 
 
 
